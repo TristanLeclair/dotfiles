65e31329451182729a9a7744ff5123e89ee5057a
TristanLeclair <60434271+TristanLeclair@users.noreply.github.com>
2024-12-06 21:45:17 -0500

    chore: temporarily disable trailspace
    - till discussion post is answered about disabling trailspace

--- nvim/lua/plugins/mini.lua
+++ nvim/lua/plugins/mini.lua
@@ -2,8 +2,14 @@ local M = { "echasnovski/mini.nvim", version = false }
 
 function M.config()
   require("mini.pairs").setup()
-  require("mini.trailspace").setup()
+  -- require("mini.trailspace").setup()
   require("mini.surround").setup()
 end
+
+local f = function(args)
+  vim.b[args.buf].minitrailspace_disable = true
+end
+vim.api.nvim_create_autocmd("Filetype", { pattern = "snacks_dashboard", callback = f })
+vim.api.nvim_create_autocmd("Filetype", { pattern = "lua", callback = f })
 
 return M
