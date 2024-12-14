09a360b45e197a766f73c9b5342536acddf7bc1c
TristanLeclair <60434271+TristanLeclair@users.noreply.github.com>
2024-12-14 01:46:09 -0500

    fix: try fixing autocommand

--- nvim/lua/tlecla/autocommand.lua
+++ nvim/lua/tlecla/autocommand.lua
@@ -4,9 +4,23 @@ vim.cmd([[
   augroup end
 ]])
 
+local config_path = vim.fn.stdpath("config")
+local data_path = vim.fn.stdpath("data")
+local path_separator = package.config:sub(1, 1)
+
+local scripts_dir = "scripts"
+local script_file = "copy_from_db_cache.sh"
+
+local db_ui_dir = "db_ui"
+
+local function create_path(paths)
+  return table.concat(paths, path_separator)
+end
+
 local group = vim.api.nvim_create_augroup("AutoRepoUpdate", { clear = true })
-local script_path = vim.fn.stdpath("config") .. "/scripts/copy_from_db_cache.sh"
-local db_ui_path = os.getenv("HOME") .. "/.local/share/db_ui/"
+local script_path = create_path({ config_path, scripts_dir, script_file })
+
+local db_ui_path = create_path({ data_path, db_ui_dir }) .. path_separator
 
 vim.api.nvim_create_autocmd("BufWritePost", {
   pattern = db_ui_path .. "*/*",
