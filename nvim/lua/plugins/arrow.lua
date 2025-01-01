cff70fa9e0c4813fa857655954fe2507fba24c07
Tristan Leclair-Vani <60434271+TristanLeclair@users.noreply.github.com>
2025-01-01 16:01:15 -0500

    fix: add default config for arrow

--- nvim/lua/plugins/arrow.lua
+++ nvim/lua/plugins/arrow.lua
@@ -1,5 +1,15 @@
 local M = {
   "otavioschwanck/arrow.nvim",
+  dependencies = {
+    { "nvim-tree/nvim-web-devicons" },
+    -- or if using `mini.icons`
+    -- { "echasnovski/mini.icons" },
+  },
+  opts = {
+    show_icons = true,
+    leader_key = ";", -- Recommended to be a single key
+    buffer_leader_key = "m", -- Per Buffer Mappings
+  },
 }
 
 return M
